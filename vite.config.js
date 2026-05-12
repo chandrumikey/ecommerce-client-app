23825025a39e6c24fa507d7a6098672caf7ca3cf
chandrumikey <142077565+chandrumikey@users.noreply.github.com>
2026-05-12 07:50:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,   // Increase limit from 500 KB to 1000 KB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Group vendor libraries into one chunk
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+        }
+      }
+    }
+  }
 })
